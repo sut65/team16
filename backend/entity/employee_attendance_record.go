8d45fac32cfdd2e70950d1788f5d0a1beb74f58d
B6326395 <B6326395@gmail.com>
2023-01-07 16:32:53 +0700

    Create Record_employee_leave - close #84

--- backend/entity/employee_attendance_record.go
+++ backend/entity/employee_attendance_record.go
@@ -9,26 +9,26 @@ import (
 type Duty struct {
 	gorm.Model
 	Name string
-	// Employee_attendance  []Employee_attendance  `gorm:"foreignKey:Duty_ID"`
-	// record_employee_leave  []record_employee_leave  `gorm:"foreignKey:Duty_ID"`
+	Employee_attendance  []Employee_attendance  `gorm:"foreignKey:Duty_ID"`
+	Record_employee_leave  []Record_employee_leave  `gorm:"foreignKey:Duty_ID"`
 }
 
 type Overtime struct {
 	gorm.Model
 	Name string
-	// Employee_attendance  []Employee_attendance  `gorm:"foreignKey:Duty_ID"`
-	// record_employee_leave  []record_employee_leave  `gorm:"foreignKey:Duty_ID"`
+	Employee_attendance  []Employee_attendance  `gorm:"foreignKey:Overtime_ID"`
+	Record_employee_leave  []Record_employee_leave  `gorm:"foreignKey:Overtime_ID"`
 }
 
 type Working_time struct {
 	gorm.Model
 	Name    string
 	WT_Time string
-	// Employee_attendance  []Employee_attendance  `gorm:"foreignKey:Duty_ID"`
-	// record_employee_leave  []record_employee_leave  `gorm:"foreignKey:Duty_ID"`
+	Employee_attendance  []Employee_attendance  `gorm:"foreignKey:Working_time_ID"`
+	Record_employee_leave  []Record_employee_leave  `gorm:"foreignKey:Working_time_ID"`
 }
 
-type employee_attendance struct {
+type Employee_attendance struct {
 	gorm.Model
 	Employee_ID     *uint
 	Employee        Employee
@@ -40,5 +40,19 @@ type employee_attendance struct {
 	Overtime        Overtime
 	Time_IN         time.Time
 	Status_ID       bool
+	Number_Em       string
+}
+
+type Record_employee_leave struct {
+	gorm.Model
+	Employee_ID     *uint
+	Employee        Employee
+	Duty_ID         *uint
+	Duty            Duty
+	Working_time_ID *uint
+	Working_time    Working_time
+	Overtime_ID     *uint
+	Overtime        Overtime
+	Time_OUT         time.Time
 	Number_Em       string
 }
