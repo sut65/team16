7ad37109dfe2f85d30c195ef97004581dbf2c06f
B6326395 <B6326395@gmail.com>
2023-01-07 15:48:44 +0700

    Create Entity Working_time - close #70

--- backend/entity/employee_attendance_record.go
+++ backend/entity/employee_attendance_record.go
@@ -20,3 +20,11 @@ type Overtime struct {
 	// record_employee_leave  []record_employee_leave  `gorm:"foreignKey:Duty_ID"`
 }
 
+
+type Working_time struct {
+	gorm.Model
+	Name      string
+	WT_Time		string
+	// Employee_attendance  []Employee_attendance  `gorm:"foreignKey:Duty_ID"`
+	// record_employee_leave  []record_employee_leave  `gorm:"foreignKey:Duty_ID"`
+}
