ce507d9be0b77d3239380a1fd0a5df67eae376e0
tunt7 <thanadetpfg@gmail.com>
2023-01-07 16:16:16 +0700

    add entity discount - close #13

--- backend/entity/discount.go
+++ backend/entity/discount.go
@@ -1,12 +1,28 @@
 package entity
 
 import (
-	_"time"
+	"time"
 
 	"gorm.io/gorm"
 )
 
+// ยังไม่เชื่อมFKกับ Inventory และ Employee
 type Discount struct {
 	gorm.Model
-	Name      string
+	Discount_Price float64
+	Discount_s     time.Time
+	Discount_e     time.Time
+
+	Discount_Type_ID *uint
+	Discount_Type    Discount_Type
+	Employee_ID      *uint
+	Employee         Employee
+	Inventory_ID     *uint
+	Inventory        Inventory
+}
+
+type Discount_Type struct {
+	gorm.Model
+	Type_Name string
+	Discount  []Discount `gorm:"foreignKey:Discount_Type_ID"`
 }
