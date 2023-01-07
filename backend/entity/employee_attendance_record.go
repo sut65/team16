e378e40c4ea716a0aff07c778147dd0e658c0430
B6326395 <B6326395@gmail.com>
2023-01-07 16:04:32 +0700

    Create Entity employee_attendance - close #72

--- backend/entity/employee_attendance_record.go
+++ backend/entity/employee_attendance_record.go
@@ -1,30 +1,44 @@
 package entity
 
 import (
-	_"time"
+	"time"
 
 	"gorm.io/gorm"
 )
 
 type Duty struct {
 	gorm.Model
-	Name      string
+	Name string
 	// Employee_attendance  []Employee_attendance  `gorm:"foreignKey:Duty_ID"`
 	// record_employee_leave  []record_employee_leave  `gorm:"foreignKey:Duty_ID"`
 }
 
 type Overtime struct {
 	gorm.Model
-	Name      string
+	Name string
 	// Employee_attendance  []Employee_attendance  `gorm:"foreignKey:Duty_ID"`
 	// record_employee_leave  []record_employee_leave  `gorm:"foreignKey:Duty_ID"`
 }
 
-
 type Working_time struct {
 	gorm.Model
-	Name      string
-	WT_Time		string
+	Name    string
+	WT_Time string
 	// Employee_attendance  []Employee_attendance  `gorm:"foreignKey:Duty_ID"`
 	// record_employee_leave  []record_employee_leave  `gorm:"foreignKey:Duty_ID"`
+}
+
+type employee_attendance struct {
+	gorm.Model
+	Employee_ID     *uint
+	Employee        Employee
+	Duty_ID         *uint
+	Duty            Duty
+	Working_time_ID *uint
+	Working_time    Working_time
+	Overtime_ID     *uint
+	Overtime        Overtime
+	Time_IN         time.Time
+	Status_ID       bool
+	Number_Em       string
 }
