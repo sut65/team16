ab9960adb6d6638f0a8e08e858290f840bb6f72e
B6326395 <B6326395@gmail.com>
2023-01-08 12:03:56 +0700

    modify entity employee - close #108

--- backend/entity/schema.go
+++ backend/entity/schema.go
@@ -12,8 +12,10 @@ type Employee struct {
 	Password string
 	Position string
 
-	Stock     []Stock     `gorm:"foreignKey:Employee_ID"`
-	Inventory []Inventory `gorm:"foreignKey:Employee_ID"`
-	Member    []Member    `gorm:"foreignKey:Employee_ID"`
-	Leave     []Leave     `gorm:"foreignKey:Employee_ID"`
+	Stock                 []Stock                 `gorm:"foreignKey:Employee_ID"`
+	Inventory             []Inventory             `gorm:"foreignKey:Employee_ID"`
+	Member                []Member                `gorm:"foreignKey:Employee_ID"`
+	Leave                 []Leave                 `gorm:"foreignKey:Employee_ID"`
+	Employee_attendance   []Employee_attendance   `gorm:"foreignKey:Employee_ID"`
+	Record_employee_leave []Record_employee_leave `gorm:"foreignKey:Employee_ID"`
 }
