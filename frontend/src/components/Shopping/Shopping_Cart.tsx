d28821276d62f8aa69c4fdc92a1d788d89047ca2
B6304416 <natthapoln27@gmail.com>
2023-01-22 15:09:08 +0700

    t

--- frontend/src/components/Shopping/Shopping_Cart.tsx
+++ frontend/src/components/Shopping/Shopping_Cart.tsx
@@ -1,234 +1,252 @@
-import React, {useEffect, useRef, useState} from 'react'
-//import MainLayout from '../layouts/MainLayout'
-//import axios from "axios"
-//import { toast } from 'react-toastify';
-//import { ComponentToPrint } from '../components/ComponentToPrint';
-//import { useReactToPrint } from 'react-to-print';
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button';
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+import { Box, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from '@material-ui/core';
+import Stack from '@mui/material/Stack';
+import { Container, Dialog, Grid, Paper, TableContainer } from '@mui/material';
 import { EmployeeInterface } from "../../models/IEmployee";
 import { MemberInterface } from "../../models/theerawat/IMember";
 import { Shopping_CartInterface } from "../../models/Natthapon/IShopping_Cart";
 import { OrderInterface } from "../../models/Natthapon/IOrder";
 import { IShelving } from "../../models/methas/IShelving";
 
+const useStyles = makeStyles((theme) => ({
+    table: {
+        minWidth: 650,
+    },
+}));
 
-function CartPage() {
+function Order_List() {
+    const classes = useStyles();
 
-    const [products, setProducts] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    //const [cart, setCart] = useState([]);
-    const [totalAmount, setTotalAmount] = useState(0);
+    const [order, setOrder] = useState<OrderInterface[]>([]);
+    const [editOrder, setEditOrder] = useState<OrderInterface>();
+    const [deleteOrder, setDeleteOrder] = useState<OrderInterface>();
 
-    const [employee, setEmployee] = React.useState<EmployeeInterface[]>([]);
-    const [member, setMember] = React.useState<MemberInterface[]>([]);
-    const [cart, setCart] = React.useState<Shopping_CartInterface[]>([]);
-    const [order, setOrder] = React.useState<OrderInterface[]>([]);
-    const [shelving, setShelving] = React.useState<IShelving[]>([]);
-    const toastOptions = {
+    const [note, setNote] = React.useState<string>("");
 
-        autoClose: 400,
-        pauseOnHover: true,
+    const [success, setSuccess] = React.useState(false);
+    const [error, setError] = React.useState(false);
+    const [openForEdit, setOpenForEdit] = React.useState(false);
+    const [openForDelete, setOpenForDelete] = React.useState(false);
+
+    const handleClose = (                                                                        
+        event?: React.SyntheticEvent | Event,
+        reason?: string
+    ) => {
+        if (reason === "clickaway") {
+        return;
+        }
+        setSuccess(false);
+        setError(false);
+    };
+
+    const handleClickOpenForEdit = (item: OrderInterface) => {
+        setOpenForEdit(true);
+        setEditOrder(item);
+    };
+
+    const handleClickOpenForDelete = (item: OrderInterface) => {
+        setOpenForDelete(true);
+        setDeleteOrder(item);
+    };
+
+    const handleCloseForEdit = () => {
+        setOpenForEdit(false);
+    };
+
+    const handleCloseForDelete = () => {
+        setOpenForDelete(false);
+    };
+
+    function timeout(delay: number) {
+        return new Promise( res => setTimeout(res, delay) );
     }
 
-    const apiUrl = "http://localhost:8080";
-    const requestOptions = {
-        method: "GET",
-        headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-            "Content-Type": "application/json"
-        },
-    };
-
-    const getEmployee = async () => {
-        fetch(`${apiUrl}/Employees`, requestOptions)
+    const getOrder = async () => {                                 
+        const apiUrl = "http://localhost:8080/Order/"+String(localStorage.getItem("uid"));
+        const requestOptions = {
+            method: "GET",      
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+                "Content-Type": "application/json",
+            },     
+        };
+        fetch(apiUrl, requestOptions)
             .then((response) => response.json())
             .then((res) => {
                 if (res.data) {
-                    console.log(res.data)
-                    setEmployee(res.data);
+                    setOrder(res.data);
                 }
-                else { console.log("NO DATA") }
             });
     };
 
-    const getMember = async () => {
-        fetch(`${apiUrl}/Members`, requestOptions)
-            .then((response) => response.json())
-            .then((res) => {
-                if (res.data) {
-                    console.log(res.data)
-                    setMember(res.data);
-                }
-                else { console.log("NO DATA") }
-            });
-    };
-
-    const getShopping_Cart = async () => {
-        fetch(`${apiUrl}/Shopping_Carts`, requestOptions)
-            .then((response) => response.json())
-            .then((res) => {
-                if (res.data) {
-                    console.log(res.data)
-                    setCart(res.data);
-                }
-                else { console.log("NO DATA") }
-            });
-    };
-
-    const getShelving = async () => {
-        fetch(`${apiUrl}/Shelvings`, requestOptions)
-            .then((response) => response.json())
-            .then((res) => {
-                if (res.data) {
-                    console.log(res.data)
-                    setShelving(res.data);
-                }
-                else { console.log("NO DATA") }
-            });
-    };
-
-    //   const fetchProducts = async() => {
-    //     setIsLoading(true);
-    //     const result = await axios.get('products');
-    //     setProducts(await result.data);
-    //     setIsLoading(false);
-    //   }
-
-    //   const addProductToCart = async(product) =>{
-    //     // check if the adding product exist
-    //     let findProductInCart = await cart.find(i=>{
-    //       return i.id === product.id
-    //     });
-
-    //     if(findProductInCart){
-    //       let newCart = [];
-    //       let newItem;
-
-    //       cart.forEach(cartItem => {
-    //         if(cartItem.id === product.id){
-    //           newItem = {
-    //             ...cartItem,
-    //             quantity: cartItem.quantity + 1,
-    //             totalAmount: cartItem.price * (cartItem.quantity + 1)
-    //           }
-    //           newCart.push(newItem);
-    //         }else{
-    //           newCart.push(cartItem);
-    //         }
-    //       });
-
-    //       setCart(newCart);
-    //       toast(`Added ${newItem.name} to cart`,toastOptions)
-
-    //     }else{
-    //       let addingProduct = {
-    //         ...product,
-    //         'quantity': 1,
-    //         'totalAmount': product.price,
-    //       }
-    //       setCart([...cart, addingProduct]);
-    //       toast(`Added ${product.name} to cart`, toastOptions)
-    //     }
-
-    //   }
-
-    //   const removeProduct = async(product) =>{
-    //     const newCart =cart.filter(cartItem => cartItem.id !== product.id);
-    //     setCart(newCart);
-    //   }
-
-    const componentRef = useRef();
-
-    const handleReactToPrint = useReactToPrint({
-        content: () => componentRef.current,
-    });
-
-    const handlePrint = () => {
-        handleReactToPrint();
+    const updateItem = (id: number,note: string) => {
+        let data = {       //ประกาศก้อนข้อมูล
+            ID: id,                                                     
+            Note: note,      
+        };
+        const apiUrl = "http://localhost:8080/orders";                      //ส่งขอการลบ  
+        const requestOptions = {     
+            method: "PATCH",      
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+                "Content-Type": "application/json",
+            },     
+            body: JSON.stringify(data),
+        };
+      
+        fetch(apiUrl, requestOptions)                                            //ขอการส่งกลับมาเช็คว่าบันทึกสำเร็จมั้ย
+        .then((response) => response.json())      
+        .then(async (res) => {      
+            if (res.data) {
+                setSuccess(true);
+                await timeout(1000); //for 1 sec delay
+                window.location.reload();     
+            } else {
+                setError(true);     
+            }
+        });        
     }
 
-    useEffect(() => {
-        fetchProducts();
-    },[]);
+    const deleteItem = (id: number) => {
+        let data = {                                                            //ประกาศก้อนข้อมูล
+            ID: id,      
+        };
+        const apiUrl = "http://localhost:8080/order/:id";                      //ส่งขอการลบ  
+        const requestOptions = {     
+            method: "DELETE",      
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+                "Content-Type": "application/json",
+            },     
+            body: JSON.stringify(data),
+        };
+      
+        fetch(apiUrl, requestOptions)                                            //ขอการส่งกลับมาเช็คว่าบันทึกสำเร็จมั้ย
+        .then((response) => response.json())      
+        .then(async (res) => {      
+            if (res.data) {
+                setSuccess(true);
+                await timeout(1000); //for 1 sec delay
+                window.location.reload();     
+            } else {
+                setError(true);     
+            }
+        });
+    }
+
 
     useEffect(() => {
-        let newTotalAmount = 0;
-        cart.forEach(icart => {
-        newTotalAmount = newTotalAmount + parseInt(icart.totalAmount);
-        })
-        setTotalAmount(newTotalAmount);
-    },[cart])
-
-
+        getOrder(); 
+        console.log(order)  
+    }, []);
 
     return (
-        <MainLayout>
-        <div className='row'>
-            <div className='col-lg-8'>
-            {isLoading ? 'Loading' : <div className='row'>
-                {products.map((product, key) =>
-                    <div key={key} className='col-lg-4 mb-4'>
-                    <div className='pos-item px-3 text-center border' onClick={() => addProductToCart(product)}>
-                        <p>{product.name}</p>
-                        <img src={product.image} className="img-fluid" alt={product.name} />
-                        <p>${product.price}</p>
-                    </div>
+        <Box>
+            <Snackbar                                                                                 //ป้ายบันทึกสำเร็จ
+                open={success}
+                autoHideDuration={6000}
+                onClose={handleClose}
+                anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            >
+                <Alert onClose={handleClose} severity="success">              
+                    บันทึกข้อมูลสำเร็จ
+                </Alert>
+            </Snackbar>
 
-                    </div>
-                )}
-                </div>}
-        
-            </div>
-            <div className='col-lg-4'>
-                <div style={{display: "none"}}>
-                    <ComponentToPrint cart={cart} totalAmount={totalAmount} ref={componentRef}/>
-                </div>
-                <div className='table-responsive bg-dark'>
-                    <table className='table table-responsive table-dark table-hover'>
-                    <thead>
-                        <tr>
-                        <td>#</td>
-                        <td>Name</td>
-                        <td>Price</td>
-                        <td>Qty</td>
-                        <td>Total</td>
-                        <td>Action</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { cart ? cart.map((cartProduct, key) => <tr key={key}>
-                        <td>{cartProduct.id}</td>
-                        <td>{cartProduct.name}</td>
-                        <td>{cartProduct.price}</td>
-                        <td>{cartProduct.quantity}</td>
-                        <td>{cartProduct.totalAmount}</td>
-                        <td>
-                            <button className='btn btn-danger btn-sm' onClick={() => removeProduct(cartProduct)}>Remove</button>
-                        </td>
+            <Snackbar                                                                                 //ป้ายบันทึกไม่สำเร็จ
+                open={error} 
+                autoHideDuration={6000} 
+                onClose={handleClose} 
+                anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            >
+                <Alert onClose={handleClose} severity="error">
+                    บันทึกข้อมูลไม่สำเร็จ
+                </Alert>
+            </Snackbar>
 
-                        </tr>)
-
-                        : 'No Item in Cart'}
-                    </tbody>
-                    </table>
-                    <h2 className='px-2 text-white'>Total Amount: ${totalAmount}</h2>
-                </div>
-
-                <div className='mt-3'>
-                    { totalAmount !== 0 ? <div>
-                    <button className='btn btn-primary' onClick={handlePrint}>
-                        Pay Now
-                    </button>
-
-                    </div> : 'Please add a product to the cart'
-
-                    }
-                </div>
-
-
-            </div>
-        </div>
-        </MainLayout>
-    )
-    }
-
-export default CartPage
+            <Grid container sx={{ padding:1 }}>
+                <h1>My Order</h1>
+            </Grid>
+            
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="Order">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell></TableCell>
+                            <TableCell><h4>Game</h4></TableCell>
+                            <TableCell align="center"><h4>Price</h4></TableCell>
+                            <TableCell align="center"><h4>Note</h4></TableCell>
+                            <TableCell align="center"><h4>Action</h4></TableCell>
+                        </TableRow>
+                    </TableHead>
+                        <TableBody>
+                            {order.map((item) => (
+                                <TableRow key={item.ID}>
+                                    <TableCell align="center"><img src={`${item.Game.Game_Picture}`} width="300" height="150"/></TableCell>
+                                    <TableCell component="th" scope="row">{item.Game.Game_Name}</TableCell>
+                                    <TableCell align="center">{item.Game.Game_Price}</TableCell>                         
+                                    <TableCell align="center">{item.Note}</TableCell>
+                                    <TableCell align="center">
+                                        <Stack direction="column" spacing={2}>
+                                            <Button variant="outlined" color="inherit" onClick={() => handleClickOpenForEdit(item)}>
+                                                Edit
+                                            </Button>
+                                            <Button variant="contained" color="secondary" onClick={() => handleClickOpenForDelete(item)}>
+                                                Delete
+                                            </Button>                                        
+                                        </Stack>
+                                    </TableCell>
+                                        <Dialog fullWidth maxWidth="xl" open={openForEdit} onClose={handleCloseForEdit} >
+                                            <DialogTitle>{editOrder?.Game.Game_Name}</DialogTitle>
+                                            <DialogContent>
+                                                <DialogContentText>
+                                                    {editOrder?.Game.Game_description}
+                                                </DialogContentText>
+                                                <TextField
+                                                    id="outlined-basic"
+                                                    placeholder="Insert details"
+                                                    variant="outlined"
+                                                    size="medium"
+                                                    multiline={true}
+                                                    minRows={9}
+                                                    maxRows={2}
+                                                    fullWidth={true}
+                                                    defaultValue={editOrder?.Note}
+                                                    onChange={(event) => setNote(event.target.value)}
+                                                />
+                                            </DialogContent>
+                                            <DialogActions>
+                                                <Button onClick={handleCloseForEdit}>Cancel</Button>
+                                                <Button onClick={() => updateItem(editOrder?.ID||0,note)}>Save</Button>
+                                            </DialogActions>
+                                        </Dialog>
+                                        <Dialog fullWidth maxWidth="xl" open={openForDelete} onClose={handleCloseForDelete} >
+                                            <DialogTitle>DELETE</DialogTitle>
+                                            <DialogContent>
+                                                <DialogContentText>
+                                                    Are you SURE to DELETE "{item.Game.Game_Name}" from Order?
+                                                </DialogContentText>
+                                            </DialogContent>
+                                            <DialogActions>
+                                                <Button onClick={handleCloseForDelete}>Cancel</Button>
+                                                <Button color="secondary" onClick={() => deleteItem(deleteOrder?.ID||0)}>Delete</Button>
+                                            </DialogActions>
+                                        </Dialog>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                </Table>
+            </TableContainer>
+        </Box>
+  );
+}
+export default Order_List
