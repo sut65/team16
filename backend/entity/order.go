5ccbad211c3de7b430538003f9048b0afd165df5
B6304416 <natthapoln27@gmail.com>
2023-01-22 20:18:53 +0700

    update db

--- backend/entity/order.go
+++ backend/entity/order.go
@@ -8,11 +8,12 @@ type Order struct {
 	gorm.Model
 	Quantity    int
 	Total       float64
+
 	Employee_ID *uint
 	Employee    Employee
 	Member_ID   *uint
 	Member      Member
 	Shelving_ID			*uint
 	Shelving			Shelving
-	Payment     []Payment 	`gorm:"foreignKey:Shopping_Cart_ID"`
+	Payment     []Payment 	`gorm:"foreignKey:Order_ID"`
 }
