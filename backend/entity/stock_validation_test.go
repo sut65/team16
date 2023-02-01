f2dc66684a003769e614339d9cd7e9ef92c0be82
Methas Pakpoompong <89190477+MaeMethas@users.noreply.github.com>
2023-02-02 00:44:31 +0700

    Update stock_validation_test.go

--- backend/entity/stock_validation_test.go
+++ backend/entity/stock_validation_test.go
@@ -29,6 +29,81 @@ func TestStock(t *testing.T) {
 
 }
 
+func TestNameNotBlank(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	st := Stock{
+		Name:     "",
+		Amount:   100,
+		Price:    60,
+		DateTime: time.Now(),
+	}
+
+	ok, err := govalidator.ValidateStruct(st)
+
+	// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
+	g.Expect(ok).ToNot(BeTrue())
+
+	// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
+	g.Expect(err).ToNot(BeNil())
+
+	// err.Error ต้องมี error message แสดงออกมา
+	g.Expect(err.Error()).To(Equal("Name cannot be blank"))
+}
+
+func TestAmount(t *testing.T) {
+	g := NewGomegaWithT(t)
+	fixture := []int{
+		0, 1001,
+	}
+
+	for _, am := range fixture {
+		st := Stock{
+			Name:     "Me",
+			Amount:   am,
+			Price:    60,
+			DateTime: time.Now(),
+		}
+
+		ok, err := govalidator.ValidateStruct(st)
+
+		// ok ต้องไม่เป็น true แปลว่าต้องจับ error ได้
+		g.Expect(ok).ToNot(BeTrue())
+
+		// err ต้องไม่เป็น nil แปลว่าต้องจับ error ได้
+		g.Expect(err).ToNot(BeNil())
+
+		// err.Error() ต้องมี message แสดงออกมา
+		g.Expect(err.Error()).To(Equal("Amount must be in the range 1-1000"))
+	}
+}
+
+func TestPrice(t *testing.T) {
+	g := NewGomegaWithT(t)
+	fixture := []float64{
+		0, 1001,
+	}
+
+	for _, pr := range fixture {
+		st := Stock{
+			Name:     "Me",
+			Amount:   100,
+			Price:    pr,
+			DateTime: time.Now(),
+		}
+
+		ok, err := govalidator.ValidateStruct(st)
+
+		// ok ต้องไม่เป็น true แปลว่าต้องจับ error ได้
+		g.Expect(ok).ToNot(BeTrue())
+
+		// err ต้องไม่เป็น nil แปลว่าต้องจับ error ได้
+		g.Expect(err).ToNot(BeNil())
+
+		// err.Error() ต้องมี message แสดงออกมา
+		g.Expect(err.Error()).To(Equal("Price must be in the range 1-1000"))
+	}
+}
 
 func TestDateTimeMustNotBePast(t *testing.T) {
 	g := NewGomegaWithT(t)
