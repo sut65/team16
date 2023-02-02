7b623ae2ad395b7dfb56c29f97cb0b115786c449
B6304416 <natthapoln27@gmail.com>
2023-02-02 19:30:59 +0700

    validation Prices > 1 - close #48

--- backend/entity/order.go
+++ backend/entity/order.go
@@ -9,8 +9,8 @@ import (
 
 type Order struct {
 	gorm.Model
-	Quantity         int		`valid:"required~ระบุจำนวน,range(1|100)~จำนวนอยู่ในช่วง (1-100)"` // 
-	Prices           float64	`valid:"required~ระบุราคา,range(1|1000000)~ราคาอยู่ในช่วง (1-1000000)"` //1
+	Quantity         int		`valid:"required~ระบุจำนวน,range(1|100)~จำนวนอยู่ในช่วง (1-100)"` 
+	Prices           float64	`valid:"required~ระบุราคา,range(1|1000000)~ราคาอยู่ในช่วง (1-1000000)"` 
 	Shelving_ID      *uint
 	Shelving         Shelving	
 	Shopping_Cart_ID *uint		
