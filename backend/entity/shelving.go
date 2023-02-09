0e35747baf0fafb6816aac615e979e4f665373e1
Methas Pakpoompong <89190477+MaeMethas@users.noreply.github.com>
2023-02-09 18:26:11 +0700

    Update shelving.go

--- backend/entity/shelving.go
+++ backend/entity/shelving.go
@@ -1,6 +1,7 @@
 package entity
 
 import (
+	"github.com/asaskevich/govalidator"
 	"gorm.io/gorm"
 )
 
@@ -22,10 +23,16 @@ type Shelving struct {
 	Stock_ID *uint `valid:"-"`
 	Stock    Stock `gorm:"references:id" valid:"-"`
 
-	Number int `valid:"required~Number must be in the range 1-20,range(1|20)~Number must be in the range 1-20"`
+	Number int `valid:"required~Number must be in the range 1-20,range(1|20)~Number must be in the range 1-20,~Number should not start with 0"`
 
 	Cost float64 `valid:"required~Cost must be in the range 1-1000,range(1|1000)~Cost must be in the range 1-1000"`
 
 	Separation []Separation `gorm:"foreignKey:Shelving_ID"`
 	Order      []Order      `gorm:"foreignKey:Shelving_ID"`
+}
+
+func init() {
+	govalidator.TagMap["image_valid"] = govalidator.Validator(func(str string) bool {
+		return govalidator.Matches(str, "^(data:image(.+);base64,.+)$")
+	})
 }
