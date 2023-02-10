04e672a4813db9c0a5a996573751ecb2b8fdf20e
Methas Pakpoompong <B6332877@gmail.com>
2023-02-10 16:46:17 +0700

    Super Update

--- backend/entity/shelving.go
+++ backend/entity/shelving.go
@@ -23,7 +23,7 @@ type Shelving struct {
 	Stock_ID *uint `valid:"-"`
 	Stock    Stock `gorm:"references:id" valid:"-"`
 
-	Number int `valid:"required~Number must be in the range 1-20,range(1|20)~Number must be in the range 1-20"`
+	Number int `valid:"required~Number must be in the range 1-20,range(1|20)~Number must be in the range 1-20,~Number should not start with 0"`
 
 	Cost float64 `valid:"required~Cost must be in the range 1-1000,range(1|1000)~Cost must be in the range 1-1000"`
 
--- backend/entity/shelving_validation_test.go
+++ backend/entity/shelving_validation_test.go
@@ -4,7 +4,11 @@ import (
 	"testing"
 
 	"github.com/asaskevich/govalidator"
+<<<<<<< HEAD
 
+=======
+	"github.com/onsi/gomega"
+>>>>>>> 32c26d3 (superupdate)
 	. "github.com/onsi/gomega"
 )
 
@@ -24,6 +28,9 @@ func TestShelving(t *testing.T) {
 
 	// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
 	g.Expect(err).To(BeNil())
+	g.Expect(ok).To(gomega.BeFalse(), "Number should not start with 0")
+	g.Expect(err).ToNot(gomega.BeNil(), "Error should not be nil")
+	g.Expect(err.Error()).To(gomega.Equal("Number should not start with 0"))
 
 
 }
@@ -79,3 +86,24 @@ func TestCost(t *testing.T) {
 	}
 }
 
+<<<<<<< HEAD
+=======
+func TestNumberDoesNotStartWithZero(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+
+	sv := Shelving{
+		Number: 05,
+		Cost:   60.00,
+	}
+	ok, err := govalidator.ValidateStruct(sv)
+
+	// ok ต้องไม่เป็น true แปลว่าต้องจับ error ได้
+	g.Expect(ok).ToNot(BeTrue())
+
+	// err ต้องไม่เป็น nil แปลว่าต้องจับ error ได้
+	g.Expect(err).ToNot(BeNil())
+
+	// err.Error() ต้องมี message แสดงออกมา
+	g.Expect(err.Error()).NotTo(Equal('0'), "Number should not start with 0")
+}
+>>>>>>> 32c26d3 (superupdate)
